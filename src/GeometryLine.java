2d201a23128d7d9fad0ec1f269206dafb289e652
devikakarpe <devikankarpe@gmail.com>
2023-03-06 09:58:34 -0800

    added logic for comparison

--- src/GeometryLine.java
+++ src/GeometryLine.java
@@ -17,10 +17,18 @@ public class GeometryLine {
         double length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
         System.out.println("Length Of Line is:"+length);
 
-
+        System.out.println();
+        System.out.println("The result of Equality is:\n ");
         boolean result1 = x1.equals(y1);
         System.out.println("Result of equals()="+result1);
         boolean result2 = x2.equals(y2);
         System.out.println("Result of equals()="+result2);
+
+        System.out.println();
+        System.out.println("The result of Comparison is:\n ");
+        Integer res1 = Integer.compare(x1,y1);
+        System.out.println("Result of compare(x1,y1)is:"+ res1);
+        Integer res2 = Integer.compare(x2,y2);
+        System.out.println("Result of compare(x2,y2)is:"+ res2);
     }
 }
