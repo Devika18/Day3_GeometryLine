dc1f75bb6330ffbf8ed78bc72bfc6c394f4db6a1
devikakarpe <devikankarpe@gmail.com>
2023-03-06 09:33:47 -0800

    added logic for calculating length of line

--- src/GeometryLine.java
+++ src/GeometryLine.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class GeometryLine {
     public static void main (String[] args) {
         System.out.println("Welcome to Geometry Line Program");
 
+        System.out.println("Enter the value of Co-ordinates x1, y1, x2, y2: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x1: ");
+        int x1=sc.nextInt();
+        System.out.println("Enter y1: ");
+        int y1=sc.nextInt();
+        System.out.println("Enter x2: ");
+        int x2=sc.nextInt();
+        System.out.println("Enter y2: ");
+        int y2=sc.nextInt();
+        double length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("Length Of Line is:"+length);
     }
 }
